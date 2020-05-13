ec84a63c7a81a05ca43f73c6ef4527a26be5b1bf
Youssef Angri <star.angri@gmail.com>
2020-05-13 17:19:01 +0000

    Initial commit

---
+++ src/main/java/com/erme/taxeTnb/TaxeTnbApplication.java
@@ -0,0 +1,13 @@
+package com.erme.taxeTnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxeTnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxeTnbApplication.class, args);
+	}
+
+}
